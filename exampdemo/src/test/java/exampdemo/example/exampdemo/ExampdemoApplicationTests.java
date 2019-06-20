a461a408b99cbb587b35db8ed74d5a84c2b190c2
unknown <mahaboob-roshini>
2019-06-20 11:03:09 +0530

    first commit

---
+++ exampdemo/src/test/java/exampdemo/example/exampdemo/ExampdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package exampdemo.example.exampdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ src/main/java/exampdemo/example/exampdemo/ExampdemoApplication.java
@@ -0,0 +1,13 @@
+package exampdemo.example.exampdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampdemoApplication.class, args);
+	}
+
+}
